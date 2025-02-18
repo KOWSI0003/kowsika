f9685f3a866dceeb5fc28e7210612d4099697cc6
KOWSI0003 <kowsika.s0003@gmail.com>
2025-02-18 15:36:21 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function sumbitData(){
+    var name=document.getElementById('name').Value
+    var dob=document.getElementById('dob').Value
+    console.log(name+""+dob)
+    var output=document.getElementById('output');
+    output.innerText='student Name:${name1},DOB:${dob}'
+
+
+}
